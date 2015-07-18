9887e2d8254eff6d787ab667c669c0b24f551d6f
Katie Musick <kmusick314@gmail.com>
2015-07-17 21:24:46 -0700

    implementation of bst via heap

---
+++ udemy-algos-ds/heaps/App.py
@@ -0,0 +1,10 @@
+from Heap import Heap;
+
+heap = Heap();
+
+heap.insert(12);
+heap.insert(-3);
+heap.insert(23);
+heap.insert(4);
+
+heap.heapsort();
---
+++ udemy-algos-ds/heaps/Heap.py
@@ -0,0 +1,78 @@
+class Heap(object):
+
+	HEAP_SIZE = 10;
+
+	def __init__(self):
+		self.heap = [0]*Heap.HEAP_SIZE;
+		self.currentPosition = -1;
+
+	def insert(self, item):
+		if self.isFull():
+			print("Heap is full...");
+			return;
+		
+		self.currentPosition = self.currentPosition + 1;
+		self.heap[self.currentPosition] = item;
+		self.fixUp(self.currentPosition);
+
+	def fixUp(self, index):
+		parentIndex = int((index-1)/2);
+
+		while parentIndex >= 0 and self.heap[parentIndex] < self.heap[index]:
+			temp = self.heap[index];
+			self.heap[index] = self.heap[parentIndex];
+			self.heap[parentIndex] = temp;
+			index = parentIndex;
+			parentIndex = int((index - 1)/2);
+
+	def getMax(self):
+		result = self.heap[0];
+		self.currentPosition = self.currentPosition - 1;
+		self.heap[0] = self.heap[self.currentPosition];
+		del self.heap[self.currentPosition + 1];
+		self.fixDown(0, -1);
+		return result;
+
+	def fixDown(self, index, upto):
+		if upto < 0:
+			upto = self.currentPosition;
+
+		while index <= upto:
+			leftChild = 2*index+1;
+			rightChild = 2*index+2;
+
+			if leftChild <= upto:
+				childToSwap = None;
+				if rightChild > upto:
+					childToSwap = leftChild;
+				else:
+					if self.heap[leftChild] > self.heap[rightChild]:
+						childToSwap = leftChild;
+					else:
+						childToSwap = rightChild;
+
+				if self.heap[index] < self.heap[childToSwap]:
+					temp = self.heap[index];
+					self.heap[index] = self.heap[childToSwap];
+					self.heap[childToSwap] = temp;
+				else:
+					break;
+
+				index = childToSwap;
+			else:
+				break;
+
+	#O(n log n) in-place sorting
+	def heapsort(self):
+		for i in range(0, self.currentPosition+1):
+			temp = self.heap[0];
+			print("%d" % temp);
+			self.heap[0] = self.heap[self.currentPosition-i];
+			self.heap[self.currentPosition-i] = temp;
+			self.fixDown(0, self.currentPosition-i-1);
+
+	def isFull(self):
+		if self.currentPosition == Heap.HEAP_SIZE:
+			return True;
+		else:
+			return False;
